b61c03e924bdb71f75a263fd2d566f972c0a3c73
Singh <nishant.singh@softwareone.com>
2025-04-01 10:19:57 +0800

    submission

--- Greetings.py
+++
@@ -1,6 +0,0 @@
-fname = input("Enter first name: ")
-lname = input("Enter last name: ")
-
-fullname = fname + lname
-
-print (f"Hello, {fullname}! Welcome to the Python program.")
---
+++ TuteDude/Greetings.py
@@ -0,0 +1,6 @@
+fname = input("Enter first name: ")
+lname = input("Enter last name: ")
+
+fullname = fname + lname
+
+print (f"Hello, {fullname}! Welcome to the Python program.")
